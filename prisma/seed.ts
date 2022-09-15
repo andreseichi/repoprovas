6ed70837bd0a4fc35657d792e86cc73be009b7dd
andreseichi <andre_seichi@hotmail.com>
2022-09-15 02:58:01 -0300

    removed promise.all

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,43 +6,33 @@ import teacherFactory from "./factories/teacherFactory";
 import termFactory from "./factories/termFactory";
 
 async function main() {
-  await Promise.all([
-    prisma.term.upsert(termFactory(1)),
-    prisma.term.upsert(termFactory(2)),
-    prisma.term.upsert(termFactory(3)),
-    prisma.term.upsert(termFactory(4)),
-    prisma.term.upsert(termFactory(5)),
-    prisma.term.upsert(termFactory(6)),
-  ]);
+  await prisma.term.upsert(termFactory(1));
+  await prisma.term.upsert(termFactory(2));
+  await prisma.term.upsert(termFactory(3));
+  await prisma.term.upsert(termFactory(4));
+  await prisma.term.upsert(termFactory(5));
+  await prisma.term.upsert(termFactory(6));
 
-  await Promise.all([
-    prisma.category.upsert(categoryFactory("Projeto")),
-    prisma.category.upsert(categoryFactory("Prática")),
-    prisma.category.upsert(categoryFactory("Recuperação")),
-  ]);
+  await prisma.category.upsert(categoryFactory("Projeto"));
+  await prisma.category.upsert(categoryFactory("Prática"));
+  await prisma.category.upsert(categoryFactory("Recuperação"));
 
-  await Promise.all([
-    prisma.teacher.upsert(teacherFactory("Diego Pinho")),
-    prisma.teacher.upsert(teacherFactory("Bruna Hamori")),
-  ]);
+  await prisma.teacher.upsert(teacherFactory("Diego Pinho"));
+  await prisma.teacher.upsert(teacherFactory("Bruna Hamori"));
 
-  await Promise.all([
-    prisma.discipline.upsert(disciplineFactory("HTML e CSS", 1)),
-    prisma.discipline.upsert(disciplineFactory("JavaScript", 2)),
-    prisma.discipline.upsert(disciplineFactory("React", 3)),
-    prisma.discipline.upsert(disciplineFactory("Humildade", 1)),
-    prisma.discipline.upsert(disciplineFactory("Planejamento", 2)),
-    prisma.discipline.upsert(disciplineFactory("Autoconfiança", 3)),
-  ]);
+  await prisma.discipline.upsert(disciplineFactory("HTML e CSS", 1));
+  await prisma.discipline.upsert(disciplineFactory("JavaScript", 2));
+  await prisma.discipline.upsert(disciplineFactory("React", 3));
+  await prisma.discipline.upsert(disciplineFactory("Humildade", 1));
+  await prisma.discipline.upsert(disciplineFactory("Planejamento", 2));
+  await prisma.discipline.upsert(disciplineFactory("Autoconfiança", 3));
 
-  await Promise.all([
-    prisma.teacherDiscipline.upsert(teacherDisciplineFactory(1, 1)),
-    prisma.teacherDiscipline.upsert(teacherDisciplineFactory(1, 2)),
-    prisma.teacherDiscipline.upsert(teacherDisciplineFactory(1, 3)),
-    prisma.teacherDiscipline.upsert(teacherDisciplineFactory(2, 4)),
-    prisma.teacherDiscipline.upsert(teacherDisciplineFactory(2, 5)),
-    prisma.teacherDiscipline.upsert(teacherDisciplineFactory(2, 6)),
-  ]);
+  await prisma.teacherDiscipline.upsert(teacherDisciplineFactory(1, 1));
+  await prisma.teacherDiscipline.upsert(teacherDisciplineFactory(1, 2));
+  await prisma.teacherDiscipline.upsert(teacherDisciplineFactory(1, 3));
+  await prisma.teacherDiscipline.upsert(teacherDisciplineFactory(2, 4));
+  await prisma.teacherDiscipline.upsert(teacherDisciplineFactory(2, 5));
+  await prisma.teacherDiscipline.upsert(teacherDisciplineFactory(2, 6));
 }
 
 main()
