49e02ee25b4e7a45b8c111f2deec0597e9f9a0fd
andreseichi <andre_seichi@hotmail.com>
2022-09-16 00:00:50 -0300

    validation already made by joi

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -6,15 +6,6 @@ import { UserData, UserInsertData } from "../types/users";
 import { generateAccessToken } from "../utils/jwt";
 
 export async function createUser(user: UserData) {
-  const { password, confirmPassword } = user;
-
-  if (password !== confirmPassword) {
-    throw {
-      type: "BAD_REQUEST",
-      message: "Password and confirm password must be the same",
-    };
-  }
-
   const userData = { email: user.email, password: hashSync(user.password, 10) };
   const result = await insert(userData);
   if (!result) {
