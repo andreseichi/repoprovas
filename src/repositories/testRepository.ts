f9b6d1b2a368352bb97c839e551cb9fd2b6b37e6
andreseichi <andre_seichi@hotmail.com>
2022-09-17 18:20:50 -0300

    removed unused repository function

--- src/repositories/testRepository.ts
+++ src/repositories/testRepository.ts
@@ -1,19 +1,6 @@
 import { prisma } from "../database/prisma";
 import { TestInsertData } from "../types/tests";
 
-export async function find(teacherId: number, disciplineId: number) {
-  const result = await prisma.teacherDiscipline.findUnique({
-    where: {
-      teacherId_disciplineId: {
-        teacherId,
-        disciplineId,
-      },
-    },
-  });
-
-  return result;
-}
-
 export async function insert(test: TestInsertData) {
   const result = await prisma.test.create({
     data: test,
