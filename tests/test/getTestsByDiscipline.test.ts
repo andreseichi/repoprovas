1a4fa5a8b5ee82234618f3a6f9f0e9fa77ed763f
andreseichi <andre_seichi@hotmail.com>
2022-09-19 03:19:41 -0300

    tests for test by discipline

--- tests/test/getTestsByDiscipline.test.ts
+++ tests/test/getTestsByDiscipline.test.ts
@@ -11,34 +11,30 @@ afterAll(async () => {
   await prisma.$disconnect();
 });
 
-describe("Get tests by teachers => GET /tests/teacher", () => {
-  it("should return status code 200 when get tests by teacher", async () => {
+describe("Get tests by discipline => GET /tests/discipline", () => {
+  it("should return status code 200 when get tests by discipline", async () => {
     const token = tokenFactory();
 
     const response = await supertest(app)
-      .get("/tests/teacher")
+      .get("/tests/discipline")
       .set("Authorization", `Bearer ${token}`);
 
     expect(response.status).toBe(200);
   });
 
-  it("should return an array with tests by teachers", async () => {
+  it("should return an array with tests by discipline", async () => {
     const token = tokenFactory();
 
     const response = await supertest(app)
-      .get("/tests/teacher")
+      .get("/tests/discipline")
       .set("Authorization", `Bearer ${token}`);
 
     expect(response.body).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
           id: expect.any(Number),
-          name: expect.any(String),
-          categories: expect.arrayContaining([
-            expect.objectContaining({
-              tests: expect.arrayContaining([]),
-            }),
-          ]),
+          number: expect.any(Number),
+          disciplines: expect.arrayContaining([]),
         }),
       ])
     );
